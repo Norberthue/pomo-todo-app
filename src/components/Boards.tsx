76afc8b0680dd86a9ad6d40ea7c63524af6d3097
Norberthue <kokanorbert9@gmail.com>
2025-03-01 21:37:08 +0100

    update input -change template name

--- src/components/Boards.tsx
+++ src/components/Boards.tsx
@@ -83,13 +83,13 @@ const Boards = ({ addBoard, dataBoard, darkMode, deleteBoard, setDarkMode, updat
                         <Link to={data.id} className={``}>
                           <h1 className="p-2 h-[60px] text-white">{data.title}</h1>
                         </Link>) : ( 
-                        <form onSubmit={(e) => {handleChangeTemplateName(e)}} className=" text-white items-center flex gap-2">
+                        <form onSubmit={(e) => {handleChangeTemplateName(e)}} className=" text-white items-center flex gap-1">
                           <input  value={updatedBoardTitle} onChange={(e) => {setUpdatedBoardTitle(e.target.value)}} autoFocus className={` mt-1 ml-1 placeholder-white border-2 border-[#5a626957]
-                            focus:ring-2 focus:ring-blue-500  duration-100 
-                            rounded-sm pt-2 pb-2 pl-2  outline-none `}/>
-                          <div className="flex items-center  gap-2">
-                            <button type='submit' className="hover:bg-slate-600 rounded-sm flex items-center duration-200 p-2"><i className="fa-solid fa-check"></i></button>
-                            <button className="hover:bg-slate-600 rounded-sm flex items-center duration-200 p-2" onClick={() => {setTemplateId('')}}><i className="fa-solid fa-xmark"></i></button>
+                              duration-100 
+                            rounded-sm pt-1 pb-1 pl-2  outline-none `}/>
+                          <div className="flex items-center  gap-1">
+                            <button type='submit' className="hover:bg-slate-600 rounded-sm duration-200  pt-1 pb-1 pl-2 pr-2"><i className="fa-solid fa-check"></i></button>
+                            <button className="hover:bg-slate-600 rounded-sm duration-200 pt-1 pb-1 pl-2 pr-2" onClick={() => {setTemplateId('')}}><i className="fa-solid fa-xmark"></i></button>
                           </div>
                         </form>
                     )}
